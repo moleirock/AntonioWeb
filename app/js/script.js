029775c0b16a96912c0931626bd5231092f751c1
MoleiroClase <alberto.moleiro7@gmail.com>
2022-10-14 18:55:58 +0200

    Fixes/ Changed name about.js to modalWindow.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,5 @@
 import { hideHead } from "./hideHead.js";
-import { modalWindows } from "./about.js";
+import { modalWindows } from "./modalWindow.js";
 import { toggleMenu } from "./btnMenu.js";
 import { playVideo } from "./videoPlay.js";
 import { turnOnLink } from "./activeLinks.js";
